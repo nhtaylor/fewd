bc91a4ee83fb28ee568c6bd60c7e94490443d9b6
nhtaylor <nt334407@gmail.com>
2019-04-17 21:05:32 -0400

    jquery first steps

--- lesson_10_files/color_scheme_switcher_jquery/js/index.js
+++ lesson_10_files/color_scheme_switcher_jquery/js/index.js
@@ -2,15 +2,26 @@ $(document).ready(function(){
 
   $('#grayButton').click(switchGray);
   $('#whiteButton').click(switchWhite);
+  $('#blueButton').click(switchBlue);
+  $('#redButton').click(switchRed);
 
   function switchGray() {
     // The following code reads as the following:
     // "Select the body tag and add a 'class' attribute with a value of 'gray'"
-    $('body').attr('class', 'gray');
+    $('body').attr('class', 'gray'); // this replaces class w/in body
+    // $('body').addClass('gray'); // bad because it just adds a class
   }
 
   function switchWhite(){
     $('body').attr('class', 'white');
+  }
+
+  function switchBlue(){
+    $('body').attr('class', 'blue');
+  }
+
+  function switchRed(){
+    $('body').attr('class', 'red');
   }
 
   // 1) Add the code to accomodate using jQuery
