653b331f4d04f893bca70ccd0824169f311c4d58
nhtaylor <nt334407@gmail.com>
2019-04-29 21:21:52 -0400

    beginning to work w/ forms

--- lesson_12_files/hilo/js/index.js
+++ lesson_12_files/hilo/js/index.js
@@ -1,4 +1,23 @@
 $(function() {
   // add code here
 
+
+  // listen for submit event on #guess guessForm
+  $("#guessForm").submit(handleGuess);
+
+  function handleGuess(event) {
+    // prevent the default behavior of
+    // our html form so it does not
+    // try to submit form to some backend server
+
+    event.preventDefault();
+
+    console.log("submitting form");
+
+  }
+
+
+
+
+
 });
