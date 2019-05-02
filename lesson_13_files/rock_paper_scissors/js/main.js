7ae62fcacb0c24ebc845bda1a36346f8f4fe27ee
nhtaylor <nt334407@gmail.com>
2019-05-02 11:37:31 -0400

    rps game beginnings

--- lesson_13_files/rock_paper_scissors/js/main.js
+++ lesson_13_files/rock_paper_scissors/js/main.js
@@ -1,8 +1,10 @@
 $(function() {
-  // variables to keep track of the scores of 
+  // variables to keep track of the scores of
   // the user and the computer
   let userScore = 0;
   let computerScore = 0;
+  let message;
+  let options = ["rock", "paper", "scissors"];
 
   /*
     Provided code: Listen for click on all buttons
@@ -15,7 +17,7 @@ $(function() {
       Provide Code: Hard Code the computer choice (for now)
       later in the lab you will make this dynamic
     */
-    let computerChoice = "rock";
+    // let computerChoice = "rock";
 
     /*
       Provided Code: Identify the User Choice (i.e. which button they clicked)
@@ -23,6 +25,7 @@ $(function() {
     */
     let userChoice = $(this).attr('id');
     console.log('user selected: ', userChoice);
+    
 
     /*
       1. Add your logic that compares the computerChoice to
@@ -30,12 +33,59 @@ $(function() {
 
          Whoever wins the round (user or computer) should get a point
     */
+    if (userChoice === 'rock' & computerChoice === 'rock') {
+      // tie
+      message = "You played rock. The bot played rock. You tied. :|";
 
+    } else if (userChoice === 'paper' & computerChoice === 'paper') {
+      // tie
+      message = "You played paper. The bot played paper. You tied. :|";
+
+    } else if (userChoice === 'scissors' & computerChoice === 'scissors') {
+      // tie
+      message = "You played scissors. The bot played scissors. You tied. :|";
+
+    } else if (userChoice === 'rock' & computerChoice === 'scissors') {
+      // user wins
+      message = "You played rock. The bot played scissors. You win. >)";
+      userScore = userScore + 1;
+
+    } else if (userChoice === 'scissors' & computerChoice === 'paper') {
+      // user wins
+      message = "You played scissors. The bot played papers. You win. >|";
+      userScore = userScore + 1;
+
+    } else if (userChoice === 'paper' & computerChoice === 'rock') {
+      // user win
+      message = "You played paper. The bot played rock. You win. >|";
+      userScore = userScore + 1;
+
+  } else if (userChoice === 'paper' & computerChoice === 'scissors') {
+      // user lose
+      message = "You played paper. The bot played scissors. You lose. :(";
+      computerScore = computerScore + 1;
+
+  } else if (userChoice === 'scissors' & computerChoice === 'rock') {
+      // user lose
+      message = "You played scissors. The bot played rock. You lose. :(";
+      computerScore = computerScore + 1;
+
+  } else if (userChoice === 'rock' & computerChoice === 'paper') {
+      // user lose
+      message = "You played rock. The bot played paper. You lose. :(";
+      computerScore = computerScore + 1;
+
+    } else {
+      message = "Something funny's going on here";
+    }
 
     /*
       2. Display the results to the page
 
     */
+    $("#humanScore").text(userScore);
+    $("#computerScore").text(computerScore);
+    $("#status").text(message);
   }
 
 });
