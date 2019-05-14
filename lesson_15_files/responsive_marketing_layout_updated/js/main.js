1d213d9db0221b967b27e3c86689aae07ae66f61
nhtaylor <nt334407@gmail.com>
2019-05-13 21:17:09 -0400

    all lesson 15 codealong

--- lesson_15_files/responsive_marketing_layout_updated/js/main.js
+++ lesson_15_files/responsive_marketing_layout_updated/js/main.js
@@ -1,2 +1,11 @@
 $(function () {
+
+  $('#mobile-menu-button').click(function () {
+    if ($('.nav-container').hasClass('showing')) {
+      $('.nav-container').removeClass('showing').addClass('hiding')
+    } else {
+      $('.nav-container').removeClass('hiding').addClass('showing')
+    }
+  })
+
 })
